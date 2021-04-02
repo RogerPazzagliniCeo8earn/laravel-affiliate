b0dd45eeeb853a63ab6a9018370726de7f333e15
Yuriy Martini <y.martini@soluzionesoftware.com>
2021-04-02 18:15:11 +0200

    tests refactoring

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,11 @@
 
 namespace Tests;
 
+use Chumper\Zipper\ZipperServiceProvider;
+use Maatwebsite\Excel\ExcelServiceProvider;
 use SoluzioneSoftware\LaravelAffiliate\Models\Feed;
 use SoluzioneSoftware\LaravelAffiliate\Models\Product;
+use SoluzioneSoftware\LaravelAffiliate\ServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -13,5 +16,17 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
         $this->app->bind(Feed::class);
         $this->app->bind(Product::class);
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->withFactories(__DIR__.'/../../database/factories');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ExcelServiceProvider::class,
+            ServiceProvider::class,
+            ZipperServiceProvider::class,
+        ];
     }
 }
--- tests/Unit/AffiliateTest.php
+++ tests/Unit/AffiliateTest.php
@@ -2,30 +2,16 @@
 
 namespace Tests\Unit;
 
-use Chumper\Zipper\ZipperServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Maatwebsite\Excel\ExcelServiceProvider;
+use SoluzioneSoftware\LaravelAffiliate\Affiliate;
 use SoluzioneSoftware\LaravelAffiliate\Traits\ResolvesBindings;
 use Tests\TestCase;
 
+/**
+ * @see Affiliate
+ */
 class AffiliateTest extends TestCase
 {
     use RefreshDatabase;
     use ResolvesBindings;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        $this->withFactories(__DIR__.'/../../database/factories');
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            ExcelServiceProvider::class,
-            ZipperServiceProvider::class,
-        ];
-    }
 }
